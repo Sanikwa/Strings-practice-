a8328fde109b33412d1d4cc1d8f39388f029ede9
Sanikwa <bornqueen22@gmail.com>
2023-06-01 15:28:56 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// let x=15;
+// console.log(x);
+//let a=7;
+//console.log(a)
+let names=["jim", "james", "paul", "tyrone"];
+console.log(names);
+let vegetables=20;
+console.log(vegetables);
+let seed1=["carrot"]
+console.log(seed1);
+let husband=["Tihon"];
+console.log(husband)
+
+let x=12;
+let a=78
+if (x===12 && a===78) {
+    let sum=x+a;
+    console.log (sum);
+}
