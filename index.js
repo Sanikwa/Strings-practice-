f93d163ef0888cfad90268d6870fd64bd61cfefb
Sanikwa <bornqueen22@gmail.com>
2023-06-02 14:58:17 -0400

    if statement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// let x=15;
-// console.log(x);
+ //let x=15;
+ //console.log(x);
 //let a=7;
 //console.log(a)
 let names=["jim", "james", "paul", "tyrone"];
@@ -16,4 +16,88 @@ let a=78
 if (x===12 && a===78) {
     let sum=x+a;
     console.log (sum);
+}let love=["AdeiYah", "Tzadaq", "Qadash","ZakiYah"];
+console.log(love)
+
+
+let age = 18;
+if (age >18){
+    //do this 
+    console.log ("access granted");
+}
+else{
+    //do this
+    console.log ("access denied")
+}
+ 
+let wood=20; 
+
+if (wood > 20) {
+    // we have enough for a treehouse
+ console.log ("build a tree house");
+}
+else if (wood >=20)
+console.log("build a stool");
+else{ 
+    //to bad so sad
+    console.log ("no tree house");
+
+} 
+let username = "admin"
+let password = "pass614"
+
+if(username = "admin" && password == "pass614") {
+    console.log( "access granted");
+
+}
+else {
+    console.log("access denied"); 
+}
+
+let jazz = "sister"
+let Sally = "mom"
+
+if(jazz= "sister" && Sally == "mom") {
+    console.log("Bestie");
+}
+else{console.log("No go");
+}
+
+let dad = "favorite"
+let von = "brother"
+let TC = "friend"
+
+if(dad= "favorite" && von == "brother" && TC == "friend"){
+    console.log("In there");
+}
+else {console.log("No go");}
+
+
+// let w=10; 
+
+// if (w >20) {
+//     // 
+//  console.log ("positive");
+// }
+// else if (w >=50)
+// console.log("build a stool");
+// else{ 
+//     //
+//     console.log ("negative");
+
+let num = 1
+if (num >0 ){
+    console.log("negative");
+}
+else if(num <0 ) {
+    console.log("positive")
+}
+else if (num = -1){
+    console.log("on the money")
+}
+
+let student = 97 
+
+if (student >80 && student <101){
+    console.log( "you did a good job");
 }
